6bd3f3ad6ab22ae40eb8fe09f700fc000d82b471
Miguel Monasterios <miguel421909@gmail.com>
2023-10-24 11:19:59 -0400

    conexion a BD

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,45 @@
 import express from 'express';
-
-
-// router
-import userRouters from './routes/user/index' 
-
+import mysql from 'mysql';
 
 const app = express();
 
-// middlaware
-app.use(express.json())
-app.use('/user', userRouters)
-// Server setup
+// middleware
+app.use(express.json());
+
+// MySQL connection configuration
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
+
+// Connect to MySQL database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL database:', err);
+  } else {
+    console.log('Connected to MySQL database');
+  }
+});
+
+// API endpoint for ping
+app.get('/ping', (_req, res) => {
+  // Perform a simple query to the MySQL database
+  connection.query('SELECT * from users', (err, rows) => {
+    if (err) {
+      console.error('Error executing MySQL query:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      const result = rows[0].result;
+      res.send(`Ping successful. Result: ${result}`);
+    }
+  });
+});
+
+// Start the server
 const port = 5000;
-
-app.get('/ping', (_req, res)=>{
-    res.send('pong')
-})
-
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
