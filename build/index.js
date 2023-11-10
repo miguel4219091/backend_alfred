4a91796b59b42e5c1e45c84761d5758a40ca6e57
Miguel Monasterios <miguel@alfredpay.com>
2023-11-10 10:51:18 -0400

    ajustes de librerias

--- build/index.js
+++ build/index.js
@@ -1,11 +1,7 @@
 "use strict";
 var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
-    var desc = Object.getOwnPropertyDescriptor(m, k);
-    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
-      desc = { enumerable: true, get: function() { return m[k]; } };
-    }
-    Object.defineProperty(o, k2, desc);
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
 }) : (function(o, m, k, k2) {
     if (k2 === undefined) k2 = k;
     o[k2] = m[k];
@@ -39,14 +35,14 @@ const transaction_1 = __importDefault(require("./routes/transaction"));
 class Server {
     constructor() {
         this.app = express_1.application;
-        this.app = (0, express_1.default)();
+        this.app = express_1.default();
         this.config();
         this.routes();
     }
     config() {
         this.app.set('port', process.env.PORT || 3000);
-        this.app.use((0, morgan_1.default)('dev'));
-        this.app.use((0, cors_1.default)());
+        this.app.use(morgan_1.default('dev'));
+        this.app.use(cors_1.default());
         this.app.use(express_1.default.json());
         this.app.use(express_1.default.urlencoded({ extended: false }));
     }
--- build/routes/user/index.js
+++ build/routes/user/index.js
@@ -4,7 +4,7 @@ const express_1 = require("express");
 const userController_1 = require("../../controllers/user/userController");
 class UserRouter {
     constructor() {
-        this.router = (0, express_1.Router)();
+        this.router = express_1.Router();
         this.config();
     }
     config() {
